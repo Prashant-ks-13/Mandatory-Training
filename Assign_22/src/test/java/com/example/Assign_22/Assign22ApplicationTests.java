3b3eb4375b3c024e177b489817f0a7e04e0df9bb
psing196 <prashantsingh21000@gmail.com>
2023-11-23 13:05:41 +0530

    Unit Testing and logging

--- Assign_22/src/test/java/com/example/Assign_22/Assign22ApplicationTests.java
+++ Assign_22/src/test/java/com/example/Assign_22/Assign22ApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.Assign_22;
 
+import com.example.Assign_22.Entity.Student;
+
+
+;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+
 
 @SpringBootTest
 class Assign22ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String name = "Prashant";
+		int age = 23;
+		int salary = 10000;
+
+		Student student = new Student();
+		student.setName(name);
+		student.setAge(age);
+		student.setSalary(salary);
+
+		Assertions.assertEquals(name, student.getName());
+		Assertions.assertEquals(age, student.getAge());
+		Assertions.assertEquals(salary, student.getSalary());
+
 	}
 
 }
